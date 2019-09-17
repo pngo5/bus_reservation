8e6ea9de823291e94237f211b854d975359583bf
phatn <phatn@10.250.39.11>
2019-09-17 18:01:16 -0400

    Testing

--- Application Development/src/Classroom/Testing.java
+++ Application Development/src/Classroom/Testing.java
@@ -1,5 +1,13 @@
 package Classroom;
-
+import java.util.*;
 public class Testing {
-
+public static void main(String[] args) {
+	int[] array = { 102, 100, 23, 120, 111, 100};
+    int max= array[0];
+    
+	
+	System.out.println(maxValue+ secondLargest);
+	}
+	
 }
+
