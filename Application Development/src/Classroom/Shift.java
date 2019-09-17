dcdb943ca01e0df5bf79a00ca8606e1dad8971c5
colemanhaynes <colemanhaynes@colemans-mbp.wifi.gsu.edu>
2019-09-17 18:50:55 -0400

    Added comments

--- Application Development/src/Classroom/Shift.java
+++ Application Development/src/Classroom/Shift.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Shift {
 
     private static void rightRotate(int[] inputArray, int n) {
+    	//n= how many times array should shift
     
         System.out.println("Array Before Rotation: ");
          
@@ -13,15 +14,19 @@ public class Shift {
         int temp;
          
         for (int i = 1; i <= n; i++) {
+        	//loop with nested for loop, each loop represents one shift
         
             temp = inputArray[inputArray.length-1]; 
+            //stores last number in temp variable
              
             for (int j = inputArray.length-1; j > 0; j--) {
             
                 inputArray[j] = inputArray[j-1];
+                //shifts array by 1 to the right
             }
              
             inputArray[0] = temp;
+            //replaces first number in array with temp
         }
          
         System.out.println("Array After Right Rotation By "+n+" Positions: ");
@@ -32,5 +37,6 @@ public class Shift {
     public static void main(String[] args) {
     
         rightRotate(new int[] {1,2,3,4,5,6,7,8}, 2);
+        //enter array here with how many times it should shift
     }
 }
