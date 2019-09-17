c7e5f26c64697787f8ce95d651be6c767d59537f
abdulhakimdarman <abdulhakimdarman@abdulhakims-air.wifi.gsu.edu>
2019-09-17 18:17:53 -0400

    Wazzaa yall

---
+++ GroupQuestions/src/Base2toBase10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Base2toBase10 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the number: ");
+		int a = input.nextInt();
+		String b = a + "";
+		
+		int[] arr = new int[b.length()];
+		double[] exp = new double[b.length()];
+		int sum = 0;
+		
+		for(int i = b.length() - 1, f=0 ;a != 0;i--, f++, a/=10) {
+			arr[i] = a%10;
+			exp[f] = Math.pow(2, i);
+		}
+		
+	
+		for(int i = 0;i < b.length(); i++) {
+			if((exp[i] * arr[i]) != 0)
+				sum += exp[i] * arr[i];
+		}
+		
+		System.out.print(sum);
+	}
+
+}
