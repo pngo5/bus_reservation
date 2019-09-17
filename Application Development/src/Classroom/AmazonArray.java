ea54e23cb172059799d5b8c3768a6ef2b9d1c362
phatn <phatn@10.250.39.11>
2019-09-17 18:00:49 -0400

    Here you go

---
+++ Application Development/src/Classroom/AmazonArray.java
@@ -0,0 +1,21 @@
+package Classroom;
+import java.util.*;
+public class AmazonArray {
+public static void main(String[] args) {
+	int[] array = { 102, 100, 23, 120, 111, 100};
+    int maxValue = array[0];
+	int secondLargest=0;
+	for(int a=1;a< array.length;a++) {
+			if (array[a] > maxValue) {
+			    secondLargest = maxValue;
+			    maxValue = array[a];
+			} else if (array[a] > secondLargest) {
+			    secondLargest = array[a];
+			}
+		}
+	System.out.println(maxValue+ secondLargest);
+	}
+	
+}
+
+
---
+++ Application Development/src/Classroom/Testing.java
@@ -0,0 +1,5 @@
+package Classroom;
+
+public class Testing {
+
+}
