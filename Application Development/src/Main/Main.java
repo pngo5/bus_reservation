5d112d19737509fc207dfd3e7bbad51143be7bb6
phatn <phatn@10.250.43.216>
2019-11-05 18:21:14 -0500

    Added main

---
+++ Application Development/src/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.stage.*;
+public class Main {
+	Button button;
+	 public static void main(String[] args) {
+		 launch(args);
+	 }
+
+}
