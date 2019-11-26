991c538d88a9085de1382cc8ef0cf821b6010c1e
phatn <phatn@DESKTOP-03517JA>
2019-11-25 19:03:22 -0500

    Add 5 different method to check for password. This way it'll be easier for me when we get to GUI.
    THIS IS FOR PASSWORD VALIDATION.
    
    EMAIL, and SNN coming SOON

--- Application Development/src/Classroom/src/MainMenu/Registration.java
+++ Application Development/src/Classroom/src/MainMenu/Registration.java
@@ -1,7 +1,10 @@
 package Classroom.src.MainMenu;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 
- * @author abdulhakimdarman
+ * @author abdulhakimdarman and phat
  * 
  * This will be for new users
  *
@@ -74,5 +77,89 @@ public class Registration {
 	public void setSsn(String ssn) {
 		this.ssn = ssn;
 	}
+
+	/****
+	 * 
+	 * 
+	   Creating a Method to check for valid password
+	   -Less than 20 characters, no less than 4 *
+	   -Must have a special characters *
+	   -Must have a number*
+	   -Must have a Upper-case letter*
+	   -Must not have space*
+	   
+	 * 
+	 * 
+	 */
+	public static  boolean lengthPasswordCheck(String newPassword) {
+		if ( newPassword.length() < 20 && newPassword.length()>=4) {  
+		  return true;	
+		} 
+		else {		
+		return false;
+      	}
+	}
+	/*****
+	 * 
+	 * 
+	 Using the pack java Regex API ;
+	 Pattern.compile and matcher;
+	 *
+	 I created a pattern instance, this is to see if the String
+	 newPassword have any special characters.
+	 *
+	 Pattern.matcher see if any of the pattern instance match the String.
+	 *
+	 (http://tutorials.jenkov.com/java-regex/pattern.html#pattern-matches)
+	 * @param newPassword
+	 * @return
+	 */
+	public static boolean  hasSpecialCharacter(String newPassword) {
+		Pattern special = Pattern.compile ("[^a-zA-Z]");
+		Matcher hasSpecial = special.matcher(newPassword);
+		return hasSpecial.find();		
+	}
+	/**
+	 * 
+	 Making a a Pattern instance for uppercase letters.
+	 Using matcher hasUpper to check if there's any uppercase letter in
+	 newPassword.
+	 Using hasUpper.find() to see if there is an Uppercase letter in the string
+	 if there is return true, if not return false
+	 * 
+	 * **/
+	 public static boolean hasUpperCase(String newPassword) {
+		 Pattern upperCase = Pattern.compile("[A-Z]");
+		 Matcher hasUpper = upperCase.matcher(newPassword);
+		 return hasUpper.find();
+		 
+	 }
+	 
+	 /***
+	  * 
+	  This method check to see if this string have a digit
+	  * @param newPassword
+	  * @return
+	  */
+	 public static boolean hasDigit(String newPassword) {
+		 Pattern digit = Pattern.compile("[0-9]");
+		 Matcher hasDigit = digit.matcher(newPassword);
+		 return hasDigit.find();
+		 
+	 }
+	 
+	 /**
+	  * Checking if there's space in the String 
+	  *
+	  * @param newPassword
+	  * @return
+	  */
+	 public static boolean hasSpace(String newPassword) {
+		  Pattern space = Pattern.compile("[ ]");
+		  Matcher hasSpace = space.matcher(newPassword);
+		  return hasSpace.find();
+	 }
+	
+	
 	
 }
