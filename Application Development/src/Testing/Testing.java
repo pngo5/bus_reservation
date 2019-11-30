7a702d3d0c64499df0b8be9f89f4dda2680cbb95
phatn <phatn@DESKTOP-03517JA>
2019-11-30 17:21:43 -0500

    Adding JavaFx library

--- Application Development/src/Testing/Testing.java
+++ Application Development/src/Testing/Testing.java
@@ -1,10 +1,33 @@
 package Testing;
-
-public class Testing {
-	public static void main(String[] args) {
-		StringBuilder str = new StringBuilder("dlroW olleH ");
-		StringBuilder reverseStr = str.reverse();
-		System.out.println(reverseStr.toString()); 
-	}
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class Testing extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }
-
