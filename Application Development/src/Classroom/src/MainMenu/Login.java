136a0c8ce1fb177640d623942ecd336408a52548
abdulhakimdarman <abdulhakimdarman@localhost>
2019-12-01 21:04:54 -0500

    adjusting the window size

--- Application Development/src/Classroom/src/MainMenu/Login.java
+++ Application Development/src/Classroom/src/MainMenu/Login.java
@@ -27,7 +27,7 @@ public class Login extends Application {
 	
 	Stage window;
 	
-	Scene adminLogin, userLogin;
+	Scene adminScene, userScene;
 	
 	Button adminLoginButton, userLoginButton , switchToUser, switchToAdmin;
 	
@@ -50,44 +50,85 @@ public class Login extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		
 		window = primaryStage;
+
+		GridPane userGrid = new GridPane();
+		userGrid.setPadding(new Insets(10, 10, 10, 10));
+		userGrid.setVgap(8);
+		userGrid.setHgap(10);
 		
-		window.setTitle("The Bus Resvoir");
-		
-		GridPane grid = new GridPane();
-		grid.setPadding(new Insets(10, 10, 10, 10));
-		grid.setVgap(8);
-		grid.setHgap(10);
-		
+		GridPane adminGrid = new GridPane();
+		adminGrid.setPadding(new Insets(10, 10, 10, 10));
+		adminGrid.setVgap(8);
+		adminGrid.setHgap(10);
 		
 		//Name label
 		Label nameLabel = new Label("Username: ");
 		GridPane.setConstraints(nameLabel, 0, 0);
 		
+		//Name label for admin
+		Label nameLabelAdmin = new Label("Username: ");
+		GridPane.setConstraints(nameLabelAdmin, 0, 0);
+		
+		
 		//Name input
 		TextField nameInput = new TextField("adarman1");
 		GridPane.setConstraints(nameInput, 1, 0);
 		
-		//Pass Label
+		TextField nameInputAdmin = new TextField("adarman1");
+		GridPane.setConstraints(nameInputAdmin, 1, 0);
+	
+		//Pass Label for User
 		Label passLabel = new Label("Password: ");
 		GridPane.setConstraints(passLabel, 0, 1);
 		
-		
+		//Pass Label for Admin
+		Label passLabelAdmin = new Label("Password: ");
+		GridPane.setConstraints(passLabelAdmin, 0, 1);
+				
 		//Password Input
 		TextField passInput = new TextField();
 		passInput.setPromptText("password");
 		GridPane.setConstraints(passInput, 1, 1);
 		
+		//Password Input
+		TextField passInputAdmin = new TextField();
+		passInputAdmin.setPromptText("password");
+		GridPane.setConstraints(passInputAdmin, 1, 1);
+				
+		
 		Button loginButton = new Button("Log In");
 		GridPane.setConstraints(loginButton, 1, 2);
 		
-		grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton);
+		Button loginButtonAdmin = new Button("Log In");
+		GridPane.setConstraints(loginButtonAdmin, 1, 2);
 		
-		Scene scene = new Scene(grid, 300, 200);
+		//Button to log into admin
+		switchToAdmin = new Button("Admin Login");
+		GridPane.setConstraints(switchToAdmin, 2, 1);
+		switchToAdmin.setOnAction(e -> {
+			window.setScene(adminScene);
+			window.setTitle("Admin Login");
+		});
 		
-		window.setScene(scene);
-		
-		window.show();
-		
-	}
 
+		//Button to switch to user
+		switchToUser = new Button("User Login");
+		GridPane.setConstraints(switchToUser, 2, 1);
+		switchToUser.setOnAction(e -> {
+			window.setScene(userScene);
+			window.setTitle("User Login");
+		});
+		
+		userGrid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton, switchToAdmin);
+		userScene = new Scene(userGrid, 300, 300);
+		
+		
+		adminGrid.getChildren().addAll(nameLabelAdmin, nameInputAdmin, passLabelAdmin, passInputAdmin, loginButtonAdmin, switchToUser);
+		adminScene = new Scene(adminGrid, 300, 300);
+		
+		
+		window.setScene(userScene);
+		window.setTitle("User Login");
+		window.show();
+	}
 }
