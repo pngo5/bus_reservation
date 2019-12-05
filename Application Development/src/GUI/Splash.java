fb98c0034e579bad6fd3653274930cdad710fcfb
Phat <Phat@DESKTOP-JRG10TU>
2019-12-05 03:59:41 -0500

    Splash pages

--- Application Development/src/GUI/Splash.java
+++ Application Development/src/GUI/Splash.java
@@ -1,5 +1,26 @@
 package GUI;
 
-public class Splash {
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+import javax.swing.SwingConstants;
 
+public class Splash {
+ 	
+	public  static void main(String[] args) {
+		
+      JWindow window = new JWindow();
+      window.getContentPane().add(new JLabel("",new ImageIcon("/Users/Phat/Desktop/giphy.gif"),SwingConstants.CENTER));
+      window.setBounds(450,300,320,250);
+      window.setVisible(true);
+      try {
+    	 Thread.sleep(3000);    	  
+         }
+   catch(InterruptedException e) {}	
+	window.dispose();
+	
+	
 }
+	
+
+	}
