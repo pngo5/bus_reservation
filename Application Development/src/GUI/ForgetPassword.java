d6151e0a758781f1152e232bfa78f46c5cee684c
Phat <Phat@DESKTOP-JRG10TU>
2019-12-05 00:26:23 -0500

    Adding.css and also forgetpassword scene

---
+++ Application Development/src/GUI/ForgetPassword.java
@@ -0,0 +1,32 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+public class ForgetPassword extends Application {
+ 	Stage window;
+ 	
+     
+
+ public static void main(String[] args) {
+	 launch(args);
+ }
+
+
+
+@Override
+public void start(Stage primaryStage) throws Exception {
+	// TODO Auto-generated method stub
+	
+}
+}
