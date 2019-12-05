a10ab454f26a7e576ca484f995c2e94828fc9678
abdulhakimdarman <abdulhakimdarman@localhost>
2019-12-05 04:10:04 -0500

    updating transition

--- Application Development/src/GUI/Splash.java
+++ Application Development/src/GUI/Splash.java
@@ -5,22 +5,40 @@ import javax.swing.JLabel;
 import javax.swing.JWindow;
 import javax.swing.SwingConstants;
 
-public class Splash {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Splash extends Application {
  	
-	public  static void main(String[] args) {
+	public  static void main(String[] args) throws Exception {
 		
-      JWindow window = new JWindow();
-      window.getContentPane().add(new JLabel("",new ImageIcon("/Users/Phat/Desktop/giphy.gif"),SwingConstants.CENTER));
-      window.setBounds(450,300,320,250);
-      window.setVisible(true);
-      try {
-    	 Thread.sleep(3000);    	  
-         }
-   catch(InterruptedException e) {}	
-	window.dispose();
+    launch(args);
 	
-	
+
 }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		 JWindow window = new JWindow();
+	      window.getContentPane().add(new JLabel("",new ImageIcon("/Users/Phat/Desktop/giphy.gif"),SwingConstants.CENTER));
+	      window.setBounds(450,300,320,250);
+	      window.setVisible(true);
+	      try {
+	    	 Thread.sleep(3000);    	  
+	         }
+	   catch(InterruptedException e) {}	
+		window.dispose();
+		
+		MainMenu mm = new MainMenu();
+		
+		try {
+		mm.start(primaryStage);
+		}catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}	
 	
 
 	}
