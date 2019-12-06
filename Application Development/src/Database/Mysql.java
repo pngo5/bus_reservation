77aa90282f16b7bfda50ae64a84469577aec3095
abdulhakimdarman <abdulhakimdarman@localhost>
2019-12-06 17:12:15 -0500

    ..

--- Application Development/src/Database/Mysql.java
+++ Application Development/src/Database/Mysql.java
@@ -1,11 +1,29 @@
 package Database;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 public class Mysql {
-/****
- * 
- * 
- * hihihi hello
- * 
- * 
- */
+	public static void main(String[] args) throws Exception {
+		getConnection();
+		 
+	}
+	
+	
+	
+	public static Connection getConnection() throws Exception  {
+		try {
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://34.74.172.98:3306/eastern-moment-259921:us-east1:cis3270-server";
+			String username = "root";
+			String password = "cis3270";
+			
+			Class.forName(driver);
+			
+			Connection conn = DriverManager.getConnection(url, username, password);
+			
+			return conn;
+		}catch(Exception e) {System.out.println(e);}
+		
+		return null;
+	}
+
 }
