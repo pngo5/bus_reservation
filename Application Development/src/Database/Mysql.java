4b00c562b31bae00e30f8cb73a16f242b6d93095
abdulhakimdarman <abdulhakimdarman@localhost>
2019-12-06 18:36:39 -0500

    creating connection

--- Application Development/src/Database/Mysql.java
+++ Application Development/src/Database/Mysql.java
@@ -1,6 +1,8 @@
 package Database;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 public class Mysql {
 	public static void main(String[] args) throws Exception {
 		getConnection();
@@ -11,14 +13,20 @@ public class Mysql {
 	
 	public static Connection getConnection() throws Exception  {
 		try {
-			String driver = "com.mysql.jdbc.Driver";
-			String url = "jdbc:mysql://34.74.172.98:3306/eastern-moment-259921:us-east1:cis3270-server";
+			String url = "jdbc:mysql://34.74.172.98/bus_database";
 			String username = "root";
 			String password = "cis3270";
 			
-			Class.forName(driver);
-			
 			Connection conn = DriverManager.getConnection(url, username, password);
+			
+			System.out.println("Connection successful");
+			
+			Statement statement = conn.createStatement();
+			ResultSet rst = statement.executeQuery("select user from mysql.user;");
+			
+			while (rst.next()) {
+				System.out.println(rst.getString(1));
+			}
 			
 			return conn;
 		}catch(Exception e) {System.out.println(e);}
