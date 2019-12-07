7df65ca7b838b245b067362d87266da2b4c0ef9b
Phat <Phat@DESKTOP-JRG10TU>
2019-12-07 02:45:20 -0500

    Adding more things to it, gotta make sure UI is okay

--- Application Development/src/GUI/AlertBox.java
+++ Application Development/src/GUI/AlertBox.java
@@ -4,6 +4,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -15,6 +16,7 @@ public class AlertBox {
 		window.initModality(Modality.APPLICATION_MODAL);
 		window.setTitle(title);
 		window.setMinWidth(250);
+		window.getIcons().add(new Image("icon.png"));
 		
 		
 		Label label = new Label();
